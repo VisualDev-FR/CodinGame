14e67b8ddac48e96ba1661aedf003ab0cbc81e30
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-04-24 18:30:37 +0200

    Update Unary_Short.java

--- Unary/Unary_Short.java
+++ Unary/Unary_Short.java
@@ -1,1 +1,23 @@
-import java.util.*;class Solution {public static void main(String z[]){char a[]=new Scanner(System.in).nextLine().toCharArray();String b="",c=b;int d=-1;for(int g:a){b+=String.format("%"+7+"s",Integer.toBinaryString(g)).replaceAll(" ", "0");}List<String>e=new ArrayList<String>();for(int f:b.toCharArray()){if(f!=d){if(c!=""){e.add(c);}e.add(f=='0'?"00":"0");c="0";d=f;}else{c+="0";}}System.out.println(String.join(" ",e)+" "+c);}}
+import java.util.*;
+
+class Solution {
+
+    public static void main(String z[]) {
+
+        char MESSAGE[] = new Scanner(System.in).nextLine().toCharArray();
+        String binaryStr="",mStr=binaryStr;
+
+        for (int mChar:MESSAGE){
+            binaryStr+=String.format("%"+7+"s",Integer.toBinaryString(mChar)).replaceAll(" ", "0");
+        }
+
+        for(String mBin:binaryStr.split("(?<=(.))(?!\\1)")){
+            mStr=mStr!=""?mStr+" ":mStr;
+            mStr+=(mBin.charAt(0)=='1'?"0 ":"00 ")+"0".repeat(mBin.length());
+        }
+        System.out.println(mStr);
+    }
+
+}
+//Sortcutted:
+//import java.util.*;class Solution{public static void main(String z[]){char a[]=new Scanner(System.in).nextLine().toCharArray();String b="",d=b;for(int e:a){b+=String.format("%"+7+"s",Integer.toBinaryString(e)).replaceAll(" ","0");}for(String c:b.split("(?<=(.))(?!\\1)")){d=d!=""?d+" ":d;d+=(c.charAt(0)=='1'?"0 ":"00 ")+"0".repeat(c.length());}System.out.println(d);}}
