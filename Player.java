d41e4d10f740ba92b81d68ee57c8e671bab239f0
vDev <menanteau.thomas@yahoo.fr>
2022-05-21 13:17:45 +0200

    Resistance : Mise en place des fonctions de base

---
+++ Player.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Solution {
+
+    static TreeMap<Character, String> traductions;
+
+    static String morseSequence;
+    
+    static int dictionaryCount;
+
+    static String[] morseWords;
+    static int[] lengthTable;    
+
+    public static void main(String args[]) {
+
+        InitializeLetters();
+        ReadInputs();
+
+        System.out.println("answer");
+    }
+
+    static String TraduceToMorse(String word){
+        
+        String finalString = "";
+
+        for(int i = 0; i < word.length(); i++){
+            
+            char currentLetter = word.charAt(i);
+
+            finalString += traductions.get(currentLetter); 
+        }
+
+        return finalString;
+    }
+
+    static void ReadInputs(){
+
+        Scanner in = new Scanner(System.in);
+
+        morseSequence = in.next();        
+        dictionaryCount = in.nextInt();
+
+        morseWords = new String[dictionaryCount];
+        lengthTable = new int[dictionaryCount];
+        
+        for (int i = 0; i < dictionaryCount; i++) {
+            morseWords[i] = TraduceToMorse(in.next());
+            lengthTable[i] = morseWords[i].length();
+        }
+    }
+
+    static void InitializeLetters(){
+
+        traductions = new TreeMap<Character, String>();
+
+        traductions.put('A', ".-"   );
+        traductions.put('B', "-..." );
+        traductions.put('C', "-.-." );
+        traductions.put('D', "-.."  );
+        traductions.put('E', "."    );
+        traductions.put('F', "..-." );
+        traductions.put('G', "--."  );
+        traductions.put('H', "...." );
+        traductions.put('I', ".."   );
+        traductions.put('J', ".---" );
+        traductions.put('K', "-.-"  );
+        traductions.put('L', ".-.." );
+        traductions.put('M', "--"   );
+        traductions.put('N', "-."   );
+        traductions.put('O', "---"  );
+        traductions.put('P', ".--." );
+        traductions.put('Q', "--.-" );
+        traductions.put('R', ".-."  );
+        traductions.put('S', "..."  );
+        traductions.put('T', "-"    );
+        traductions.put('U', "..-"  );
+        traductions.put('V', "...-" );
+        traductions.put('W', ".--"  );
+        traductions.put('X', "-..-" );
+        traductions.put('Y', "-.--" );
+        traductions.put('Z', "--.." );
+
+    }    
+}
+
