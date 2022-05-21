b59fbc2c69623b9aeaa7f92dd8c6bee6d6984c0d
vDev <menanteau.thomas@yahoo.fr>
2022-05-21 17:05:37 +0200

    Resistance : Mise en place du système de console locale

--- Player.java
+++ Player.java
@@ -8,28 +8,35 @@ class Solution {
     static int dictionaryCount;
 
     static String[] morseWords;
-    static int[] lengthTable;    
+    static int[] lengthTable;   
 
     public static void main(String args[]) {
 
         InitializeLetters();
-        ReadInputs();
+        //ReadInputs();
+        long answer = GetValidator();
 
-        int totalCombinaisons = GetCominaisons(morseSequence);
+        long totalCombinaisons = GetCominaisons(morseSequence);
 
-        System.out.println(totalCombinaisons);
+        System.out.println("Answer : " + answer + " / Found : " + totalCombinaisons);
+
     }
 
     static int GetCominaisons(String morseSequence){
+
+        Debug(morseSequence);
+        Debug(" ");
         
         int totalCombinaisons = 0;
 
         for(String word : morseWords){
 
-            if(morseSequence.indexOf(word)>-1){
+            Debug(word);
+
+            if(morseSequence.indexOf(word) > -1){
                 
                 if(morseSequence.length() - word.length() == 0){
-                    
+
                     totalCombinaisons++;
                 
                 }else{
@@ -108,5 +115,93 @@ class Solution {
     static void Debug(String message){
         System.err.println(message);
     }
+
+    //Functions for local tests
+
+    static long GetValidator(){
+
+        System.out.println("Please choose a Validator to run : ");
+
+        int number = new Scanner(System.in).nextInt();
+
+        switch (number){
+
+            case 1: return Validator_1();
+            case 2: return Validator_2();
+            case 3: return Validator_3();
+            case 5: return Validator_5();
+            case 6: return Validator_6();
+            default : return GetValidator();
+        }
+    }
+
+    static void ParseValidator(String[] unicodeWords){
+
+        morseWords = new String[unicodeWords.length];
+        lengthTable = new int[unicodeWords.length];
+        
+        for (int i = 0; i < morseWords.length; i++) {
+            morseWords[i] = TraduceToMorse(unicodeWords[i]);
+            lengthTable[i] = morseWords[i].length();
+        }        
+
+    }
+
+    static long Validator_1(){
+
+        morseSequence = "-.-";            
+        String[] unicodeWords = "A B C HELLO K WORLD".split(" ");
+
+        ParseValidator(unicodeWords);
+
+        return 1;
+
+    }
+
+    static long Validator_2(){
+
+        morseSequence = "--.-------..";            
+        String[] unicodeWords = "GOD GOOD MORNING G HELLO".split(" ");
+
+        ParseValidator(unicodeWords);
+
+        return 1;
+
+    }
+
+    static long Validator_3(){
+
+        morseSequence = "......-...-..---.-----.-..-..-..";            
+        String[] unicodeWords = "HELL HELLO OWORLD WORLD TEST".split(" ");
+
+        ParseValidator(unicodeWords);
+
+        return 2;
+
+    }
+    
+    static long Validator_5(){
+
+        morseSequence = "-.-..---.-..---.-..--";            
+        String[] unicodeWords = "CAT KIM TEXT TREM CEM".split(" ");
+
+        ParseValidator(unicodeWords);
+
+        return 125;
+
+    }    
+
+    static long Validator_6(){
+
+        morseSequence = "..............................................";            
+        String[] unicodeWords = "E I".split(" ");
+
+        ParseValidator(unicodeWords);
+
+        return 2971215073L;
+
+    }       
+
+
 }
 
