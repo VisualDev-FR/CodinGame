6dacc615410d47be5ceefa9a38a7606e6744e78f
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-05-26 14:35:02 +0200

    Resistance : Implémentation d'un système de comptage dynamique des séquences Fonctionnel à 100% / très performant mais semble pouvoir être optimisé

--- Player.java
+++ Player.java
@@ -4,104 +4,62 @@ import java.io.*;
 class Solution {
 
     static TreeMap<Character, String> traductions;
-    static TreeMap<String, Sequence> encounteredSequences;
-    static TreeMap<Integer, Node> nodes;
 
     static String morseInput;    
     static int dictionaryCount;
-    static Matrix adjacentMatrix;
 
-    static long branchesCount = 0;
-
-    static String[] morseWords;
-    static int[] lengthTable;   
+    static String[] morseWords; 
     static long totalCombinaisons;
-    static boolean nullMatrix;
     static long nbIterations;
-    static int matrixSize;
+    static String[] unicodeWords;
+
 
     public static void main(String args[]) {
 
-        Initialize();
         LocalSession(0);
         //OnlineSession();        
     }
 
     static void CountCombinaisons(){
 
-        nbIterations = 0;
-        matrixSize = morseInput.length() + 1;
+        Initialize();
 
-        Matrix matProd = adjacentMatrix;
+        List<String> morseSequences = new ArrayList<String>();
 
-        totalCombinaisons = adjacentMatrix.Get(matrixSize-1, 0);
-
-        for(int i = 0; i < matrixSize;  i++){
-
-            //matProd.Print();
-
-            matProd = ProdMat(adjacentMatrix, matProd);
-            totalCombinaisons += matProd.Get(matrixSize - 1, 0);
-
-            if(nullMatrix) return;
-        }
-    }
-
-    static Matrix ProdMat(Matrix matrixA, Matrix matrixB){
-
-        Matrix matProd = new Matrix();
-
-        List<String> intersections = new ArrayList<String>();
-
-        for(int row : matrixA.rows.keySet()){
+        for(String word : unicodeWords){            
             
-            for(int column : matrixA.rows.get(row)){
+            String traduction = TraduceToMorse(word);
 
-                if(matrixB.rows.containsKey(column)){
-                
-                    for(int mColumn : matrixB.rows.get(column)){
+            if(!morseSequences.contains(word)){
+                morseSequences.add(traduction);
+            }
+            
+        }
 
-                        nbIterations++;
-    
-                        String interKey = "" + row + ":" + column + ":" + column + ":" + mColumn;
-    
-                        if(!intersections.contains(interKey)){
+        long[] countTable = new long[morseInput.length()+1];
+        countTable[morseInput.length()] = 1;
 
-                            long increaseValue = matrixA.Get(row, column) * matrixB.Get(column, mColumn);
-                            
-                            nullMatrix = increaseValue == 0L && nullMatrix;
+        morseWords = morseSequences.toArray(new String[0]);
 
-                            matProd.Increase(row, mColumn, increaseValue);                            
-    
-                            intersections.add(interKey);
-                        }
-                    }
-                }
-    
-                if(matrixB.columns.containsKey(row)){
-    
-                    for(int mRow : matrixB.columns.get(row)){
+        for(int i = morseInput.length()-1; i >= 0; i--){
 
-                        nbIterations++;
-    
-                        String interKey = "" + mRow + ":" + row + ":" + row + ":" + column;
-                        
-                        if(!intersections.contains(interKey)){
+            long acc = 0;
+            
+            for(int j = 0; j < morseWords.length ; j++){
 
-                            long increaseValue = matrixA.Get(mRow, row) * matrixB.Get(row, column);
-                            
-                            nullMatrix = increaseValue == 0L && nullMatrix;
+                nbIterations++;
 
-                            matProd.Increase(mRow, column, increaseValue);                             
+                if(morseInput.startsWith(morseWords[j], i)){
 
-                            intersections.add(interKey);
-                        }
-                    }
+                    acc += countTable[i + morseWords[j].length()];
                 }
             }
+            countTable[i] = acc;
+            System.err.printf("countTable[%s] = %s\n", i, acc);
         }
 
-        return matProd;
+        totalCombinaisons = countTable[0];
+
     }
 
     static String TraduceToMorse(String word){
@@ -118,26 +76,9 @@ class Solution {
         return finalString;
     }
 
-    static void ReadInputs(){
-
-        Scanner in = new Scanner(System.in);
-
-        morseInput = in.next();        
-        dictionaryCount = in.nextInt();
-
-        for (int i = 0; i < dictionaryCount; i++) {
-            new Sequence(in.next());          
-        }
-    }
-
     static void Initialize(){
 
-        totalCombinaisons = 0;
-        branchesCount = 0;
-
-        adjacentMatrix = new Matrix();
-        encounteredSequences = new TreeMap<String, Sequence>();
-        nodes = new TreeMap<Integer, Node>();        
+        totalCombinaisons = 0;    
         traductions = new TreeMap<Character, String>();
 
         traductions.put('A', ".-"   );
@@ -169,196 +110,32 @@ class Solution {
 
     }
 
-    static void Debug(String message){
-        System.err.println(message);
-    }
-    
-    //Class
+    //Online Sessions
 
-    public static class Sequence{
+    static void ReadInputs(){
 
-        public List<int[]> branches;
-        public List<Integer> indexes;
+        Scanner in = new Scanner(System.in);
 
-        public String morseSequence;
-        public int length;
-        
-        public Sequence(String word){
+        morseInput = in.next();        
+        dictionaryCount = in.nextInt();
 
-            morseSequence = TraduceToMorse(word);
-            length = morseSequence.length();
+        unicodeWords = new String[dictionaryCount];
 
-            if(encounteredSequences.containsKey(morseSequence)){
-
-                branches = new ArrayList<int[]>();
-                indexes = new ArrayList<Integer>();
-
-                this.Duplicate(encounteredSequences.get(morseSequence));
-
-            }else{
-
-                encounteredSequences.put(morseSequence, this);
-
-                branches = new ArrayList<int[]>();
-                indexes = new ArrayList<Integer>();
-                
-                int i = 0;
-                int begIndex = morseInput.indexOf(morseSequence, i);
-
-                while(begIndex > -1){
-
-                    if(begIndex > -1 && !indexes.contains(begIndex)){
-
-                        indexes.add(begIndex);
-    
-                        int endIndex = begIndex + length;
-    
-                        Node beginNode = nodes.containsKey(begIndex) ? nodes.get(begIndex) : new Node(nodes.size(), begIndex);
-                        Node endNode = nodes.containsKey(endIndex) ? nodes.get(endIndex) : new Node(nodes.size(), endIndex);
-                        
-                        beginNode.Connect(endNode);                    
-                        branches.add(new int[]{begIndex, endIndex});
-                        branchesCount++;
-                    }
-
-                    i = morseInput.indexOf(morseSequence, i) + 1;
-                    begIndex = morseInput.indexOf(morseSequence, i);
-
-                }
-            }
-        }
-
-        public void Duplicate(Sequence mSequence){
-
-            for(int[] branch : mSequence.branches){
-
-                branchesCount++;
-                
-                Node begNode = nodes.get(branch[0]);
-                Node endNode = nodes.get(branch[1]);
-
-                begNode.Connect(endNode);
-
-            }
-        }
-
-        public void Print(){
-            System.err.printf("%s : ", this.morseSequence);
-
-            for(Integer index : this.indexes){
-                System.err.print("" + index + " ");
-            }
-    
-            System.err.print("\n");             
+        for (int i = 0; i < dictionaryCount; i++) {
+            unicodeWords[i] = (in.next());          
         }
     }
-
-    public static class Node{
-
-        public List<Node> nodesFrom;
-        public List<Node> nodesTo;
-        
-        public int ID;
-        public int index;
-
-        public Node(int nodeID, int nodeIndex){
-
-            ID = nodeID;
-            index = nodeIndex;
-
-            if(!nodes.containsKey(index)) nodes.put(index, this);
-        }
-
-        public void Connect(Node mNode){
-
-            Node minNode = mNode.index < this.index ? mNode : this;
-            Node maxNode = mNode.index < this.index ? this : mNode;
-
-            adjacentMatrix.Increase(maxNode.index, minNode.index, 1L);
-        }
-    }
-
-    public static class Matrix{
-
-        Map<String, Long> values;
-        Map<Integer, List<Integer>> columns;
-        Map<Integer, List<Integer>> rows;
-
-        public Matrix(){
-
-            values = new HashMap<String, Long>();
-            columns = new HashMap<Integer, List<Integer>>();
-            rows = new HashMap<Integer, List<Integer>>();
-        }
-
-        public void Increase(int row, int column, long value){
-            
-            values.put(row + ":" + column, values.getOrDefault(row + ":" + column, 0L) + value);
-
-            columns.putIfAbsent(column, new ArrayList<Integer>());
-            rows.putIfAbsent(row, new ArrayList<Integer>());
-
-            columns.get(column).add(row);
-            rows.get(row).add(column);
-
-        }
-
-        public long Get(int row, int column){
-            return values.getOrDefault(row + ":" + column, 0L);
-        }
-
-        public void Print(){
-
-            for(int i = 0; i < matrixSize; i++){
-
-                String[] arrayTemp = new String[matrixSize];
-
-                for(int j = 0; j < matrixSize; j++){
-                    arrayTemp[j] = this.Get(i, j) > 0 ? ""+this.Get(i, j) : ".";
-                }
-
-                Debug(String.join(" ", arrayTemp));
-            }
-
-            Debug(" ");
-
-        }
-
-        public void PrintTxtFile() throws FileNotFoundException, UnsupportedEncodingException{
-
-            PrintWriter writer = new PrintWriter("Matrix.txt", "UTF-8");
-            
-            //writer.println("The first line");
-            //writer.println("The second line");
-            
-            for(int i = 0; i < matrixSize; i++){
-
-                String[] arrayTemp = new String[matrixSize];
-
-                for(int j = 0; j < matrixSize; j++){
-                    arrayTemp[j] = this.Get(i, j) > 0 ? ""+this.Get(i, j) : " ";
-                }
-
-                writer.println(String.join(";", arrayTemp));
-            }            
-            
-            writer.close();
-
-        }
-    }
-
-    //Functions for switching local to online running
 
     static void OnlineSession(){
 
         ReadInputs();
 
-        matrixSize = morseInput.length() + 1;
-
         CountCombinaisons();
 
         System.out.println(totalCombinaisons);        
     }
+
+    //Local Sessions
 
     static void LocalSession(int number){
 
@@ -394,11 +171,11 @@ class Solution {
         Initialize();
 
         long answer = GetValidator(Validator);
-  
+
         CountCombinaisons();
 
         System.out.println(" ");
-        System.out.printf(" Validator %s : Answer = %s / Found = %s nbIterations = %s matrixSize = %s \n\n", Validator, answer, totalCombinaisons, nbIterations, matrixSize);
+        System.out.printf(" Validator %s : Answer = %s / Found = %s nbIterations = %s\n\n", Validator, answer, totalCombinaisons, nbIterations);
     }
 
     static long GetValidator(int number){
@@ -416,19 +193,10 @@ class Solution {
         return -1;
     }
 
-    static void ParseValidator(String[] unicodeWords){
-
-        for (int i = 0; i < unicodeWords.length; i++) {            
-            new Sequence(unicodeWords[i]);
-        }
-    }
-
     static long Validator_1(){
 
         morseInput = "-.-";            
-        String[] unicodeWords = "A B C HELLO K WORLD".split(" ");
-
-        ParseValidator(unicodeWords);
+        unicodeWords = "A B C HELLO K WORLD".split(" ");
 
         return 1;
     }
@@ -436,9 +204,7 @@ class Solution {
     static long Validator_2(){
 
         morseInput = "--.-------..";            
-        String[] unicodeWords = "GOD GOOD MORNING G HELLO".split(" ");
-
-        ParseValidator(unicodeWords);
+        unicodeWords = "GOD GOOD MORNING G HELLO".split(" ");
 
         return 1;
     }
@@ -446,9 +212,7 @@ class Solution {
     static long Validator_3(){
 
         morseInput = "......-...-..---.-----.-..-..-..";            
-        String[] unicodeWords = "HELL HELLO OWORLD WORLD TEST".split(" ");
-
-        ParseValidator(unicodeWords);
+        unicodeWords = "HELL HELLO OWORLD WORLD TEST".split(" ");
 
         return 2;
     }
@@ -461,9 +225,7 @@ class Solution {
         
         fileContent.remove(0);
 
-        String[] unicodeWords = fileContent.toArray(new String[0]);
-
-        ParseValidator(unicodeWords);
+        unicodeWords = fileContent.toArray(new String[0]);
 
         return 57330892800L; //57 330 892 800
     }
@@ -471,9 +233,7 @@ class Solution {
     static long Validator_5(){
 
         morseInput = "-.-..---.-..---.-..--";            
-        String[] unicodeWords = "CAT KIM TEXT TREM CEM".split(" ");
-
-        ParseValidator(unicodeWords);
+        unicodeWords = "CAT KIM TEXT TREM CEM".split(" ");
 
         return 125;
     }    
@@ -481,9 +241,7 @@ class Solution {
     static long Validator_6(){
 
         morseInput = "..............................................";            
-        String[] unicodeWords = "E I".split(" ");
-
-        ParseValidator(unicodeWords);
+        unicodeWords = "E I".split(" ");
 
         return 2971215073L; //2 971 215 073
     } 
