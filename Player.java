921cfbe32f9fdd02017e40fe386dbfef036c82d7
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-06-09 17:27:01 +0200

    LineRacing : Direction choisie vers le joueur le plus proche du prochain point Manque de précision due à la méthode de calcul de distance, à améliorer par un pathfinding

--- Player.java
+++ Player.java
@@ -7,6 +7,7 @@ class Player {
     static int nbOfPlayer;
     static int myPlayerID;
 
+    static LightCycle myLightCycle;
     static Map <Integer, LightCycle> lightCycles;
     static Map<String, int[]> directions;
     static Grid grid;
@@ -28,25 +29,23 @@ class Player {
 
             String bestDirection = "UP";
             int maxSurface = 0;
-            int minOppDist = 20*30;
+            int minOppDist = 9999;
 
-            //System.err.printf("myPosition = %s %s\n", lightCycles.get(myPlayerID).Y1, lightCycles.get(myPlayerID).X1);
+            System.err.printf("myPosition = %s %s\n", myLightCycle.Y1, myLightCycle.X1);
 
             for(String direction : directions.keySet()){
 
                 int[] dirCoord = directions.get(direction);
 
-                int nextRow = lightCycles.get(myPlayerID).Y1 + dirCoord[0];
-                int nextCol = lightCycles.get(myPlayerID).X1 + dirCoord[1];
-
-                int oppID = myPlayerID == 0 ? 1 : 0;
+                int nextRow = myLightCycle.Y1 + dirCoord[0];
+                int nextCol = myLightCycle.X1 + dirCoord[1];
 
                 int surface = GetDiffusedSurface(nextRow, nextCol, grid.arrayGrid);
-                int oppDist = GetDistance(nextRow, nextCol, lightCycles.get(oppID).Y1, lightCycles.get(oppID).X1);
+                int oppDist = GetCloserOpponent(nextRow, nextCol);
 
                 boolean validPosition = IsPositionValid(nextRow, nextCol, grid.arrayGrid);
 
-                System.err.printf("%s : surface = %s / valid : %s\n", direction, surface, validPosition);
+                System.err.printf("%s : surface = %s / oppDist : %s\n", direction, surface, oppDist);
 
                 if(validPosition && (surface > maxSurface || (surface == maxSurface && oppDist < minOppDist))){
                     bestDirection = direction;
@@ -55,15 +54,27 @@ class Player {
                 }
             }
 
-            int bestRow = lightCycles.get(myPlayerID).Y1 + directions.get(bestDirection)[0];
-            int bestCol = lightCycles.get(myPlayerID).X1 + directions.get(bestDirection)[1];
-
-            grid.set(bestRow, bestCol, Integer.toString(myPlayerID));
-
             System.out.println(bestDirection);
 
             firstTurn = false;
         }
+    }
+
+    // UTILITIES
+
+    static int GetCloserOpponent(int row, int col){
+
+        int minDist = 9999;
+
+        for(LightCycle lightCycle : lightCycles.values()){
+
+            if(lightCycle.ID != myPlayerID && !lightCycle.IsDead()){
+                int dist = GetDistance(row, col, lightCycle.Y1, lightCycle.X1);
+                minDist = Math.min(minDist, dist);
+            }
+        }
+
+        return minDist;
     }
 
     static int GetDiffusedSurface(int row, int col, String[][] arrayGrid){
@@ -109,57 +120,16 @@ class Player {
         boolean isValid = false;
 
         if(col >= 0 && col < 30 && row >=0 && row < 20){
-
             isValid = arrayGrid[row][col].equals(".");
-
         }
-
         return isValid;
-
-    }
-
-    static void ReadFirstInputs(Scanner in){
-
-        directions = new HashMap<String, int[]>();
-        directions.put("RIGHT", new int[]{ 0 ,  1});
-        directions.put("LEFT",  new int[]{ 0 , -1});
-        directions.put("UP",    new int[]{-1 ,  0});
-        directions.put("DOWN",  new int[]{ 1 ,  0});  
-        
-        grid = new Grid();
-
-        nbOfPlayer = in.nextInt(); // total number of players (2 to 4).
-        myPlayerID = in.nextInt(); // your player number (0 to 3).
-
-        lightCycles = new HashMap<Integer, LightCycle>();
-        
-        for (int i = 0; i < nbOfPlayer; i++){
-            
-            LightCycle mLightCycle = new LightCycle(i);
-
-            mLightCycle.Update(in);
-
-            lightCycles.put(i, mLightCycle);            
-        }
-    }
-
-    static void ReadInputs(Scanner in){
-
-        nbOfPlayer = in.nextInt(); // total number of players (2 to 4).
-        myPlayerID = in.nextInt(); // your player number (0 to 3).
-
-        System.err.printf("nbOfPlayer : %s / myPlayerID : %s\n", nbOfPlayer, myPlayerID);
-
-        for(LightCycle lightCycle : lightCycles.values()){
-
-            lightCycle.Update(in);  
-            
-        }
     }    
 
     static int GetDistance(int row1, int col1, int row2, int col2){
         return Math.abs(row1-row2) + Math.abs(col1-col2);
     }
+
+    // CLASSES
 
     public static class LightCycle{
 
@@ -181,8 +151,6 @@ class Player {
 
         public void Erase(){
 
-            //System.err.printf("Erasing player : %s / isDead = %s\n", ID, X0 == -1 && Y0 == -1 && X1 == -1 && Y1 == -1);
-
             for(int[] coord : coords){
                 grid.set(coord[1], coord[0], ".");
             }           
@@ -199,11 +167,10 @@ class Player {
             X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
             Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
 
-            System.err.printf("%s : %s %s %s %s\n", ID, X0, Y0, X1, Y1);
-
             if(!IsDead()){
                 coords.add(new int[]{X1, Y1});
                 grid.set(Y1, X1, Integer.toString(this.ID)); 
+                grid.set(Y0, X0, Integer.toString(this.ID)); 
             }else if(!erased){
                 Erase();
                 erased = true;
@@ -235,59 +202,47 @@ class Player {
         }
 
         public void Print(){
-
             for(int i = 0; i < arrayGrid.length; i++){
-
-                for(int j = 0; j < arrayGrid[0].length; j++){
-
-                    if(arrayGrid[i][j].equals(".")){
-/* 
-                        int myDist = GetDistance(i, j, lightCycles.get(myPlayerID).Y1, lightCycles.get(myPlayerID).X1);
-                        int minDist = 9999;
-                        
-                        for(LightCycle lightCycle : lightCycles.values()){
-                            if(lightCycle.ID != myPlayerID){                                
-                                minDist = Math.min(minDist, GetDistance(i, j, lightCycle.Y1, lightCycle.X1));                                
-                            }
-                        }
-                        System.err.print(myDist < minDist ? "." : (myDist == minDist ? "+" : "-")); */
-                        System.err.print(".");
-                        
-                    }else if(arrayGrid[i][j].equals(Integer.toString(myPlayerID))){
-                        //System.err.print("#");
-                        System.err.print(Integer.toString(myPlayerID));                        
-                        //System.err.printf("%s %s val = %s / myID = %s\n", i, j, exploredGrid[i][j], myPlayerID);
-                    }else{                        
-                        System.err.print(arrayGrid[i][j]);
-                    }   
-                }
-                System.err.print("\n");
+                System.err.println(String.join(" ", arrayGrid[i]));
             }
         }
     }
+    
+    // INPUTS READING
 
-    // TESTING
+    static void ReadFirstInputs(Scanner in){
 
-    static Integer[] GetCloserLightCycle(int row, int col){
-
-        List<Integer> distances = new ArrayList<Integer>();
-        int minDist = 20 * 30;
+        directions = new HashMap<String, int[]>();
+        directions.put("RIGHT", new int[]{ 0 ,  1});
+        directions.put("DOWN",  new int[]{ 1 ,  0});
+        directions.put("LEFT",  new int[]{ 0 , -1});
+        directions.put("UP",    new int[]{-1 ,  0});
         
+        grid = new Grid();
+
+        nbOfPlayer = in.nextInt(); // total number of players (2 to 4).
+        myPlayerID = in.nextInt(); // your player number (0 to 3).
+
+        lightCycles = new HashMap<Integer, LightCycle>();
+
+        for (int i = 0; i < nbOfPlayer; i++){
+            
+            LightCycle mLightCycle = new LightCycle(i);
+
+            mLightCycle.Update(in);
+            lightCycles.put(i, mLightCycle);                   
+        }
+
+        myLightCycle = lightCycles.get(myPlayerID);
+    }
+
+    static void ReadInputs(Scanner in){
+
+        nbOfPlayer = in.nextInt(); // total number of players (2 to 4).
+        myPlayerID = in.nextInt(); // your player number (0 to 3).
+
         for(LightCycle lightCycle : lightCycles.values()){
-
-            int lighDist = GetDistance(row, col, lightCycle.Y1, lightCycle.X1);
-
-            if(lighDist < minDist){
-                
-                distances = new ArrayList<Integer>();                
-                distances.add(lightCycle.ID);
-
-            }else if(lighDist == minDist){
-                
-                distances.add(lightCycle.ID);
-            }            
-        }        
-
-        return distances.toArray(new Integer[0]);
+            lightCycle.Update(in);            
+        }
     }    
 }
