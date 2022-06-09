439b0bed2875cb4d6b08217f1b40e592ec86a698
Thomas Menanteau <91687641+VisualDev-FR@users.noreply.github.com>
2022-06-09 15:03:01 +0200

    LineRacing : Passage en Ligue Argent Choix de la direction qui offre le plus de surface par diffusion

--- Player.java
+++ Player.java
@@ -25,22 +25,11 @@ class Player {
 
             if(!firstTurn) ReadInputs(in);
 
-            grid.Print();
+            //grid.Print();
 
-/* 
-            int[] reachablePositions = CountMaxReachablePositions();
-
-            for(int i = 0; i < reachablePositions.length; i++){
-                System.err.printf("%s : %s\n", i, reachablePositions[i]);
-            }
-*/
-
-            for(LightCycle lightCycle : lightCycles.values()){
-                System.err.printf("%s : %s\n", lightCycle.ID, GetDiffusedSurface(lightCycle, grid));
-            }
-
-            String bestDirection = "";
-            int bestScore = 0;
+            String bestDirection = "UP";
+            int maxSurface = 0;
+            int minOppDist = 20*30;
 
             System.err.printf("myPosition = %s %s\n", lightCycles.get(myPlayerID).Y1, lightCycles.get(myPlayerID).X1);
 
@@ -50,14 +39,21 @@ class Player {
 
                 int nextRow = lightCycles.get(myPlayerID).Y1 + dirCoord[0];
                 int nextCol = lightCycles.get(myPlayerID).X1 + dirCoord[1];
+
+                String[][] gridTemp = grid.arrayGrid.clone();
+                //gridTemp[nextRow][nextCol] = Integer.toString(myPlayerID);
                 
-                int directionScore = EvaluateScore(nextRow, nextCol, 0);
+                int oppID = myPlayerID == 0 ? 1 : 0;
 
-                System.err.printf("%s : %s\n", direction, directionScore);
+                int surface = GetDiffusedSurface(nextRow, nextCol, grid.arrayGrid);
+                int oppDist = GetDistance(nextRow, nextCol, lightCycles.get(oppID).Y1, lightCycles.get(oppID).X1);
 
-                if(directionScore > bestScore || (directionScore == bestScore && bestScore == 0)){
+                System.err.printf("%s : surface = %s\n", direction, surface);
+
+                if(IsPositionValid(nextRow, nextCol, grid.arrayGrid) && (surface > maxSurface || (surface == maxSurface && oppDist < minOppDist))){
                     bestDirection = direction;
-                    bestScore = directionScore;
+                    maxSurface = surface;
+                    minOppDist = oppDist;
                 }
             }
 
@@ -66,227 +62,51 @@ class Player {
         }
     }
 
-    static List<Node> GetMiniMaxGraph(Node startNode, int depth){
-        
-        if(depth == 0 || !IsPositionValid(startNode.row, startNode.col, startNode.GetGrid())){
+    static int GetDiffusedSurface(int row, int col, String[][] arrayGrid){
 
-            List<Node> finalNodes = new ArrayList<Node>();
+        List<int[]> borders = new ArrayList<int[]>();
+        boolean[][] visitedPoints = new boolean[20][30];
 
-            for(int[] direction : directions.values()){
+        borders.add(new int[]{row, col});
 
-                int nextRow = startNode.row + direction[0];
-                int nextCol = startNode.col + direction[1];
+        int bordersCount = 1;
 
-                finalNodes.add(new Integer[]{nextRow, nextCol});
-            }
+        while(borders.size() > 0){
 
-            return finalNodes;
+            List<int[]> bordersTemp = new ArrayList<int[]>(borders);
 
-        }else{
+            for(int[] border : bordersTemp){
 
-            for(int[] direction : directions.values()){
+                borders.remove(borders.indexOf(border));
 
-                int nextRow = lastRow + direction[0];
-                int nextCol = lastCol + direction[1];
+                for(int[] direction : directions.values()){
 
-                return GetMiniMaxGraph(nextRow, nextCol, depth-1, isMyLight);
-            }
-        }
+                    int borderRow = border[0] + direction[0];
+                    int borderCol = border[1] + direction[1];
+    
+                    if(IsPositionValid(borderRow, borderCol, arrayGrid)){
 
-        return null;
-    }
+                        if(visitedPoints[borderRow][borderCol] == false){
 
-    public static class Node{
-
-        private int row;
-        private int col;
-        private Node parent;        
-        private int playerID;
-        private int eval;
-
-        public Node(String direction, int mRow, int mCol, int mPlayerID, Node mParent){
-
-            row = mRow;
-            col = mCol;
-            parent = mParent;
-            playerID = mPlayerID;            
-        }
-
-        public void Evaluate(){
-
-            Grid gridTemp = GetGrid();
-
-            eval = GetDiffusedSurface(lightCycles.get(playerID), gridTemp);
-        }
-
-        public Grid GetGrid(){
-
-            Grid gridTemp = new Grid();
-            
-            gridTemp.exploredGrid = grid.exploredGrid.clone();
-            gridTemp.set(row, col, Integer.toString(this.playerID));
-
-            Node currentParent = this.parent;
-
-            while(currentParent != null){
-                
-                gridTemp.set(currentParent.row, currentParent.col, Integer.toString(currentParent.playerID));                
-                currentParent = currentParent.parent;
-            }
-
-            return gridTemp;
-        }
-    }    
-
-    /*
-    //initial Call
-
-    maxEval = +infinity
-    for each direction from currentPosition{
-        eval = minimax(currentPosition, 6, true, -infinity, +infinity)
-        maxEval = Math.max(maxEval, eval)
-        bestDirection = direction
-    }
-
-    Play maxEval
-
-
-
-    static String MiniMax(int[] position, int depth, int maximizingPLayer, int alpha, int beta){
- 
-        if (depth == 0 or !IsPositionValid(position[0], position[1], mGrid)){
-            return static evaluation of position
-        }
-
-        if maximizingPlayer == true{
-            
-            maxEval = -infinity
-            
-            for each child of currentPosition{
-                eval = minimax(child position, depth-1, false, alpha, beta)
-                maxEval = Math.max(maxEval, eval)
-                alpha = Math.max(alpha, eval)
-                
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            return maxEval;
-
-        }else{
-            
-            minEval = +infinity
-            
-            for each child of currentPosition{
-                
-                eval = minimax(childPosition, depth-1, true, alpha, beta)
-                minEval = Math.min(minEval, eval)
-                beta = Math.min(beta, eval)
-                
-                if(beta <= alpha){
-                    break;
-                }
-            }
-            Return minEval
-        }
-    }
-
-*/
-
-    static int[] GetBestMove(){
-
-        List<String> paths = new ArrayList<String>();
-
-        paths.add("5:10");
-
-        GenerateGraph(paths, MINIMAX_DEPTH);
-
-        for(String entry : paths){
-            System.err.printf(" %s\n", entry);
-        }
-
-        System.err.printf("\nnbVal : %s\n", paths.size());
-        
-        return null;
-    }
-
-    static void GenerateGraph(List<String> paths, int depth){
-
-        if(depth > 0){
-
-            List<String> pathsTemp = new ArrayList<String>(paths);
-
-            for(String path : pathsTemp){
-
-                if(path.split(" ").length <= MINIMAX_DEPTH){
-                    
-                    paths.remove(path);
-
-                    String[] lastPosition = path.substring(path.lastIndexOf(" ") + 1).split(":");
-        
-                    for(int[] direction : directions.values()){
-        
-                        int nextRow = Integer.parseInt(lastPosition[0]) + direction[0];
-                        int nextCol = Integer.parseInt(lastPosition[1]) + direction[1];
-
-                        String newPosition = nextRow + ":" + nextCol;                    
-                        String newPath = path + " " + newPosition;
-        
-                        if(path.indexOf(newPosition) == -1){
-                            paths.add(newPath);                        
+                            visitedPoints[borderRow][borderCol] = true;
+                            borders.add(new int[]{borderRow, borderCol});
+                            bordersCount++;
                         }
                     }
-                    GenerateGraph(paths, depth - 1); 
-                }          
-            }
-        }
-    }
-    static int[] CountMaxReachablePositions(Grid mGrid){
-
-        int[] reachablePositions = new int[nbOfPlayer];
-
-        for(int i = 0; i < mGrid.exploredGrid.length; i++){
-
-            for(int j = 0; j < mGrid.exploredGrid[0].length; j++){
-
-                if(mGrid.exploredGrid[i][j].equals(".")){
-
-                    int minDist = 9999;
-                    int minID = -1;
-                        
-                    for(LightCycle lightCycle : lightCycles.values()){
-
-                        int lighDist = GetDistance(i, j, lightCycle.Y1, lightCycle.X1);
-
-                        if(lighDist < minDist){
-                            minDist = lighDist;
-                            minID = lightCycle.ID;
-                        }                                
-                        
-                    }
-
-                    reachablePositions[minID]++;
                 }
             }
         }
-                
-        return reachablePositions;
-    }
 
-    static int EvaluateScore(int nextRow, int nextCol, int bestScore){
+        return bordersCount;
+    }    
 
-        if(!IsPositionValid(nextRow, nextCol, grid)) return 0;
-
-        return 1;
-    }
-
-    static boolean IsPositionValid(int row, int col, Grid mGrid){
+    static boolean IsPositionValid(int row, int col, String[][] arrayGrid){
 
         boolean isValid = false;
 
         if(col >= 0 && col < 30 && row >=0 && row < 20){
 
-            isValid = mGrid.get(row, col).equals(".");
+            isValid = arrayGrid[row][col].equals(".");
 
         }
 
@@ -375,38 +195,34 @@ class Player {
     
     public static class Grid{
 
-        String[][] exploredGrid;
+        String[][] arrayGrid;
         
         public Grid(){
 
-            exploredGrid = new String[20][30];
+            arrayGrid = new String[20][30];
 
-            for(int i = 0; i < exploredGrid.length; i++){
-                for(int j = 0; j < exploredGrid[0].length; j++){
-                    exploredGrid[i][j] = ".";
+            for(int i = 0; i < arrayGrid.length; i++){
+                for(int j = 0; j < arrayGrid[0].length; j++){
+                    arrayGrid[i][j] = ".";
                 }
             }
         }
 
         public String get(int row, int column){            
-            return exploredGrid[row][column];
+            return arrayGrid[row][column];
         }
 
         public void set(int row, int column, String value){
-            exploredGrid[row][column] = value;
+            arrayGrid[row][column] = value;
         }
 
         public void Print(){
 
-            for(int i = 0; i < exploredGrid.length; i++){
+            for(int i = 0; i < arrayGrid.length; i++){
 
-                for(int j = 0; j < exploredGrid[0].length; j++){
+                for(int j = 0; j < arrayGrid[0].length; j++){
 
-                    //System.err.print(exploredGrid[i][j]);
-
-                    //if(j % 5 == 0) System.err.print ("  ");
-
-                    if(exploredGrid[i][j].equals(".")){
+                    if(arrayGrid[i][j].equals(".")){
 
                         int myDist = GetDistance(i, j, lightCycles.get(myPlayerID).Y1, lightCycles.get(myPlayerID).X1);
                         int minDist = 9999;
@@ -418,11 +234,11 @@ class Player {
                         }
                         System.err.print(myDist < minDist ? "." : (myDist == minDist ? "+" : "-"));
                         
-                    }else if(exploredGrid[i][j].equals(Integer.toString(myPlayerID))){
+                    }else if(arrayGrid[i][j].equals(Integer.toString(myPlayerID))){
                         System.err.print("#");
                         //System.err.printf("%s %s val = %s / myID = %s\n", i, j, exploredGrid[i][j], myPlayerID);
                     }else{                        
-                        System.err.print(exploredGrid[i][j]);
+                        System.err.print(arrayGrid[i][j]);
                     }   
                 }
                 System.err.print("\n");
@@ -431,44 +247,6 @@ class Player {
     }
 
     // TESTING
-
-    static int GetDiffusedSurface(LightCycle mLightCycle, Grid mGrid){
-
-        List<int[]> borders = new ArrayList<int[]>();
-        boolean[][] visitedPoints = new boolean[20][30];
-
-        borders.add(new int[]{mLightCycle.Y1, mLightCycle.X1});
-
-        int bordersCount = 1;
-
-        while(borders.size() > 0){
-
-            List<int[]> bordersTemp = new ArrayList<int[]>(borders);
-
-            for(int[] border : bordersTemp){
-
-                borders.remove(borders.indexOf(border));
-
-                for(int[] direction : directions.values()){
-
-                    int borderRow = border[0] + direction[0];
-                    int borderCol = border[1] + direction[1];
-    
-                    if(IsPositionValid(borderRow, borderCol, mGrid)){
-
-                        if(visitedPoints[borderRow][borderCol] == false){
-
-                            visitedPoints[borderRow][borderCol] = true;
-                            borders.add(new int[]{borderRow, borderCol});
-                            bordersCount++;
-                        }
-                    }
-                }
-            }
-        }
-
-        return bordersCount;
-    }
 
     static Integer[] GetCloserLightCycle(int row, int col){
 
@@ -493,5 +271,3 @@ class Player {
         return distances.toArray(new Integer[0]);
     }    
 }
-
-
